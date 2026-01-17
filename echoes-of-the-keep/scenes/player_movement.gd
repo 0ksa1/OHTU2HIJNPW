bae8c3faba043a4923e89b97425fa7e9c8481fe5
MiskaHalonen <156885943+MiskaHalonen@users.noreply.github.com>
2026-01-17 19:13:30 +0200

    fysics update
    fysiikat uusiksi sopimaan meidän peli ideaan itse koodaamalla

---
+++ echoes-of-the-keep/scenes/player_movement.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var mov_speed : float = 500
+var character_direction : Vector2
+
+func _physics_process(delta) :
+	character_direction.x = Input.get_axis("move_left", "move_right")
+	character_direction.y = Input.get_axis("move_up", "move_down")
+	character_direction = character_direction.normalized()
+	
+	# kääntyminen
+	if character_direction.x > 0 : %sprite.flip_h = false
+	elif character_direction.x < 0 : %sprite.flip_h = true
+	
+	if character_direction:
+		velocity = character_direction * mov_speed
+		if %sprite.animation != "running": %sprite.animation = "running"
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, mov_speed)
+		if %sprite.animation != "Idle" : %sprite.animation = "Idle"
+		
+	move_and_slide()
