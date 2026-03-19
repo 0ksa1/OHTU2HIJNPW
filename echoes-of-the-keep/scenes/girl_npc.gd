ac0a0ec225591c412dc20b737a956c9d7f40fe69
0ksa1 <aliisapikka5@gmail.com>
2026-03-19 22:22:07 +0200

    Implemented very basic version of cutscene

--- echoes-of-the-keep/scenes/girl_npc.gd
+++ echoes-of-the-keep/scenes/girl_npc.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
+@onready var cutscene_manager = $"../CutsceneManager"
+
 var player = null
 var player_in_chat_zone = false
 
 @onready var dialogue = $CanvasLayer/Dialogue
+
+func _ready():
+	print("CutsceneManager node:", $"../CutsceneManager")  # adjust path
 
 func _on_chat_detection_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -24,3 +29,4 @@ func _start_dialoque():
 
 func _on_dialogue_dialogue_finished():
 	print("Dialogue finished")
+	cutscene_manager.start_cutscene()
---
+++ echoes-of-the-keep/scripts/cutscene_manager.gd
@@ -0,0 +1,31 @@
+extends Node
+
+@onready var player = $"../player"
+@onready var player_camera = $"../player/Camera2D"
+@onready var cutscene_camera = $"../cutscene_camera"
+var cutscene_active = false
+
+func pan_camera(target_position: Vector2):
+	var tween = create_tween()
+	tween.tween_property(
+		cutscene_camera,
+		"position",
+		target_position,
+		2.0
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	await tween.finished
+
+func start_cutscene():
+	cutscene_active = true
+	player_camera.enabled = false
+	cutscene_camera.enabled = true
+	
+	await pan_camera(Vector2(500, 200))
+	await get_tree().create_timer(2.0).timeout
+
+	end_cutscene()
+
+func end_cutscene():
+	cutscene_camera.enabled = false
+	player_camera.enabled = true
+	cutscene_active = false
