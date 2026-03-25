51b551d389dcb877f9997e1da7048a507c9ac994
Joni Niiranen <joni43@hotmail.fi>
2026-03-25 16:23:01 +0200

    Ruudun värin päivitys

--- echoes-of-the-keep/scenes/inventory_slot.gd
+++ echoes-of-the-keep/scenes/inventory_slot.gd
@@ -6,9 +6,12 @@ extends Panel
 
 var ItemClass = preload("res://item.tscn")
 var item = null
+var slot_index: int = -1
 
 func _ready() -> void:
+	#print("inventory slot testi")
 	clear_slot()
+	border_color()
 
 func clear_slot() -> void:
 	if item != null:
@@ -20,7 +23,9 @@ func set_item(item_id: StringName) -> void:
 	item = ItemClass.instantiate()
 	add_child(item)
 
+
 	if item is Control:
+		item.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		item.anchor_left = 0.0
 		item.anchor_top = 0.0
 		item.anchor_right = 1.0
@@ -31,3 +36,10 @@ func set_item(item_id: StringName) -> void:
 		item.offset_bottom = 0
 
 	item.set_item(item_id)
+
+#funktio itse slotin värin vaihtoon
+func border_color() -> void:
+	if Inventory.selected_slot == slot_index:
+		modulate = Color.ANTIQUE_WHITE
+	else:
+		modulate = Color.WHITE
