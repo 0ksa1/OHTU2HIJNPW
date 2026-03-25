f292abd12610243cf3ce9a52e7edaf52a1bf4f12
Joni Niiranen <joni43@hotmail.fi>
2026-03-25 16:23:01 +0200

    Ruudun värin päivitys

--- echoes-of-the-keep/scripts/autoloads/inventory.gd
+++ echoes-of-the-keep/scripts/autoloads/inventory.gd
@@ -8,6 +8,9 @@ signal inventory_updated
 
 var inventory: Array = []
 
+# valitun slotin ns indeksi, -1 sillä mitään slottia ei ole vielä valittuna
+var selected_slot: int = -1
+
 func _ready() -> void:
 	#inventory alustus kun 12 paneelia
 	inventory.resize(12)
@@ -15,10 +18,11 @@ func _ready() -> void:
 		inventory[i] = null
 		
 	#testi jolla lisäsin itemejä tavaraluetteloon
-	#add_item(&"potion")
-	#add_item(&"potion") testi
+	add_item(&"potion")
+	add_item(&"potion") 
 	#add_item(&"potion")
 
+#itemin lisäys
 func add_item(item_id: StringName) -> bool:
 	for i in range(inventory.size()):
 		if inventory[i] == null:
@@ -26,6 +30,19 @@ func add_item(item_id: StringName) -> bool:
 			inventory_updated.emit()
 			return true
 	return false
+
+#kahden itemin paikkojen vaihto, vaihdetaan indekseissä olevat itemit päittäin
+func swap_items(index_a: int, index_b: int) -> void:
+	if index_a < 0 or index_a >= inventory.size():
+		return
+	if index_b < 0 or index_b >= inventory.size():
+		return
+
+	var temp = inventory[index_a]
+	inventory[index_a] = inventory[index_b]
+	inventory[index_b] = temp
+
+	inventory_updated.emit()
 
 # tähän voisi lisätä sitten remove item kun potion tms käytetään
 
