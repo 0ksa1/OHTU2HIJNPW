79a85fded034be470b4e66318b5e5a62fd5a5642
0ksa1 <aliisapikka5@gmail.com>
2026-03-31 22:46:38 +0300

    SCRUM-114 spawnausehdot dungeonissa Upon death player respawns in checkpoints in the dungeon

---
+++ echoes-of-the-keep/scripts/autoloads/spawn_controller.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var rats_dead: bool = false
+var bats_dead: bool = false
+var slimes_dead: bool = false
+
+var current_spawn: String = "DefaultSpawn"
+
+func update_spawn():
+	if bats_dead and slimes_dead:
+		current_spawn = "dungeon_bossroom_sp"
+	elif rats_dead:
+		current_spawn = "dungeon_room2_sp"
+	else:
+		current_spawn = "dungeon_room1_sp"
+		
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	pass
